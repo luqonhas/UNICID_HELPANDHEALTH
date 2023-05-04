b4303b519a35edcc17acdce37b86fa7f1f80c934
luqonhas <apolinariodev@gmail.com>
2023-05-03 21:40:22 -0300

    primeira parte

---
+++ ChatApp - HELP&HEALTH/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Chat App</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+  
+  <!-- Favicon -->
+  <link href="img/favicon.ico" rel="icon">
+
+<!-- Google Web Fonts -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"> 
+
+<!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+<!-- Libraries Stylesheet -->
+<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+<!-- Customized Bootstrap Stylesheet -->
+<link href="css/style.css" rel="stylesheet">
+</head>
---
+++ ChatApp - HELP&HEALTH/javascript/login.js
@@ -0,0 +1,43 @@
+const form = document.querySelector(".login form"),
+continueBtn = form.querySelector(".button input"),
+errorText = form.querySelector(".error-text");
+
+form.onsubmit = (e)=>{
+    e.preventDefault();
+}
+
+continueBtn.onclick = ()=>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/login.php", true);
+    xhr.onload = ()=>{
+      if(xhr.readyState === XMLHttpRequest.DONE){
+          if(xhr.status === 200){
+              let data = xhr.response;
+              if(data === "success"){
+                location.href = "users.php";
+              }else{
+                errorText.style.display = "block";
+                errorText.textContent = data;
+              }
+          }
+      }
+    }
+    let formData = new FormData(form);
+    xhr.send(formData);
+}
+const progressBar = document.querySelector('.progress-bar');
+const submitBtn = document.querySelector('input[type="submit"]');
+
+submitBtn.addEventListener('click', function() {
+  progressBar.style.display = 'block';
+  let value = 0;
+  let progressInterval = setInterval(function() {
+    if (value >= 100) {
+      clearInterval(progressInterval);
+      window.location.href = 'users.php';
+    } else {
+      value++;
+      progressBar.value = value;
+    }
+  }, 30);
+});
